631db1438d0b9479159c29044c22fe1b66df36d7
Ian Crosser <iancrosser@gmail.com>
2018-03-23 16:39:28 -0400

    Adding Java Run Time to the docker install script.

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y libexpat1-dev libboost-dev libboost-filesystem-dev swig
+apt-get install -y default-jre libexpat1-dev libboost-dev libboost-filesystem-dev swig
 ./autogen.sh
 ./configure --disable-dotnet --disable-java --disable-perl --disable-php --disable-python --disable-python3 --disable-node.js
 make
