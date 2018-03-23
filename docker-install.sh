f243a92edf9ab6a00eede4fc4241a7141b4602fe
Ian Crosser <iancrosser@gmail.com>
2018-03-23 16:33:17 -0400

    Adding an install script for docker.

---
+++ docker-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y libexpat1-dev libboost-dev libboost-filesystem-dev swig
+./autogen.sh
+./configure --disable-dotnet --disable-java --disable-perl --disable-php --disable-python --disable-python3 --disable-node.js
+make
+make install
